ff7933e070f5db8d1081de7c2128627d694fc4ce
phil <pmgoddard89@gmail.com>
2015-01-21 20:44:17 +0000

    uploaded run_analysis.R and updated the readme. added codebook.md

--- README.md
+++ README.md
@@ -1,14 +1,32 @@
-# get_clean_data_coursework
+# getting and cleaning data coursework
 ## coursework for coursera getting and cleaning data
-## note: package dplyr MUST be installed
+## note: package dplyr MUST be installed to run script
+## refer to the codebook for description of data and analysis performed
 
 The script run_analysis.R should be run after setting the working
 directory to the directory that contains the (unzipped) UCI
 HAR Dataset directory
 
-One file, avg_data will be written after running the script, which 
+One file, tidy_avg.txt will be written after running the script, which 
 contains the average of each reading for each subject for each activity.
-This can be easily read back into R (ensuring that header=TRUE) when read
+This can be easily read back into R (ensuring that header=TRUE when read)
 
 After running the R script, the only data frame which exists is my_data, which
 is the data set produced from following steps 1-4 
+
+The code works by:
+
+- clean up global environment, load dplyr
+- loading the files of interest
+- cbinding the training data and test data seperately to two single data frames (use dplyr tbl_df)
+- rbinding the test and train data frames to have one complete data frame, my_data
+- add the names to the column headings (from features.txt)
+- remove duplicate columns
+- select only column headings containing subject, activity, mean( and std(
+- arrange the data frame by subject
+- put a descriptive activity name in activity column
+- clean up column headings
+- my_data is now complete, and is retained. this is steps 1-4 in instructions
+- to do step 5, set up a dplyr chain
+- this chain uses group_by to group by subject and activity, then uses summarise_each to find the mean of the columns for the group. assigns to data frame tidy_averaged
+- rename the columns to reflect they are now an average, then write to file tidy_avg.txt. Remove the data frame tidy_averaged from the global environment
---
+++ run_analysis.R
@@ -0,0 +1,116 @@
+library(dplyr)
+rm(list=ls())
+
+## some path names. run this script in the directory that contains
+## the UCI HAR Dataset directory
+s_test <- "UCI HAR Dataset/test/subject_test.txt"
+X_test <- "UCI HAR Dataset/test/X_test.txt"
+y_test <- "UCI HAR Dataset/test/y_test.txt"
+s_train <- "UCI HAR Dataset/train/subject_train.txt"
+X_train <- "UCI HAR Dataset/train/X_train.txt"
+y_train <- "UCI HAR Dataset/train/y_train.txt"
+col_names <- "UCI HAR Dataset/features.txt"
+activity_names <- "UCI HAR Dataset/activity_labels.txt"
+
+
+## read the raw path
+s_test_data <- read.table(s_test)
+X_test_data <- read.table(X_test)
+y_test_data <- read.table(y_test)
+s_train_data <- read.table(s_train)
+X_train_data <- read.table(X_train)
+y_train_data <- read.table(y_train)
+col_names <- read.table(col_names)
+activity_names <- read.table(activity_names)
+
+
+# merge train to single df. use dplyr tbl_df for readability
+train_dat <- s_train_data
+train_dat <- cbind(train_dat,y_train_data)
+train_dat <- cbind(train_dat,X_train_data)
+train_dat <- tbl_df(train_dat)
+#dimensions of train_dat are 7352 rows, 563 columns
+
+# merge test
+test_dat <- s_test_data
+test_dat <- cbind(test_dat,y_test_data)
+test_dat <- cbind(test_dat,X_test_data)
+test_dat <- tbl_df(test_dat)
+#dimensions of test_dat are 2947  563
+
+#merge two sets
+my_data <- rbind(train_dat,test_dat)
+#dimensions of data are 10299 rows, 563 columns
+
+# name column headings
+names(my_data)[1:2] <- c("subject","activity")
+names(my_data)[3:563] <- as.character(col_names[,2])
+
+# tidy up- remove non-needed junk
+rm(s_test,X_test,y_test,s_train,X_train,y_train,
+   s_test_data,X_test_data,y_test_data,s_train_data,
+   X_train_data,y_train_data,train_dat,test_dat,col_names)
+
+## now seem to have some duplicate column names. just remove.
+my_data <- my_data[,unique(colnames(my_data))]
+
+# now have data nicely combined into a data frame
+# next step is to extract only those columnswith 'mean' or 'std' in name
+my_data <- select(my_data,matches("subject|activity|mean\\(|std\\("))
+# my_data now has dimension 10299 rows, 66 columns
+# NOTE did not select any of the meanFreq(), only the mean() and std()
+
+#lets tidy up and arrange by subject
+my_data <- arrange(my_data,subject)
+
+#now we want to use descriptive name for activity. activity labels.txt
+#tells us:
+# 1 WALKING
+# 2 WALKING_UPSTAIRS
+# 3 WALKING_DOWNSTAIRS
+# 4 SITTING
+# 5 STANDING
+# 6 LAYING
+
+# the labels for activity are integers- convert to factor then
+# read in levels from activity_labels.txt
+my_data <- mutate(my_data,activity=as.factor(activity))
+levels(my_data$activity) <- activity_names[,2]
+rm(activity_names)
+
+
+#convert to character, lower case
+my_data <- mutate(my_data,activity=tolower(as.character(activity)))
+
+# now time to tidy up those nasty headings. 
+# replace '-' with '.'
+names(my_data) <- gsub("\\-","\\.",names(my_data))
+# remove parenthesis
+names(my_data) <- gsub("\\(\\)","",names(my_data))
+#remove duplicate "bodybody" in some column names
+names(my_data) <- gsub("bodybody","body",names(my_data))
+# cast as lower case
+names(my_data) <- tolower(names(my_data))
+
+# the above is steps 1:4. Step 5 can be done with a dplyr one-liner
+# to make averaged for each subject and each activity:
+tidy_averaged <- my_data %>% 
+    group_by(subject,activity)%>% 
+    summarise_each(funs(mean))
+
+#rename relevant columns
+names(tidy_averaged) <- gsub("fbody","avg\\.fbody",names(tidy_averaged))
+names(tidy_averaged) <- gsub("tbody","avg\\.tbody",names(tidy_averaged))
+
+#write to tidy_avg.txt in current directory
+write.table(tidy_averaged, "tidy_avg.txt",row.name=FALSE)
+
+# contains 180 rows (180 subjects),
+#68 columns (66 readings + activity label + subject label)
+
+rm(tidy_averaged)
+
+# to read back in, use the commands
+# test_read <- read.table(file="tidy_avg.txt",header=TRUE)
+# test_read <- tbl_df(test_read)
+# head(test_read,20)
